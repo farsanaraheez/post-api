7680bed1d0931bbd0362c74c6765da98e36a4927
farsanaraheez <farsanaraheez@gmail.com>
2023-03-06 16:41:50 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { HttpClient} from '@angular/common/http'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'postapi';
+  title="postapi";
+  constructor( private http:HttpClient){}
+     onSubmit(data: any){
+      this.http.post('https://jsonplaceholder.typicode.com/posts',data)
+      .subscribe((result)=>{
+      console.log("result", +result);
+     })
+
+    }
+    
+ 
+    
 }
